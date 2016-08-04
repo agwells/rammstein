88253a78f89794417fae166ad035687feb4cfc47
Aaron Wells <aaronw@catalyst.net.nz>
2016-08-05 10:24:54 +1200

    As seen on the internet

---
+++ doclass.cpp
@@ -0,0 +1,23 @@
+do
+do class
+do class if.
+do
+do class
+do class if.
+
+do!
+do class!
+do class if
+do class if
+do class if!
+do class if inline
+do class if inline
+do class if inline
+bool this delete define!
+
+this do int break sizeof public
+try if struct for auto static...
+while!
+while!
+
+// by Rammstein
